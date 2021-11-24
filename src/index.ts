314509e43bf06035b5f222a633781c181eb9db03
Eric Park <80627536+Bokdol11859@users.noreply.github.com>
2021-11-24 23:56:11 +0900

    Class learned

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,21 @@
-interface Human {
-  name: string;
-  gender: string;
-  age: number;
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
 
-const person = {
-  name: "Eric",
-  gender: "male",
-  age: 21,
-};
+const eric = new Human("Eric", 21, "male");
 
 const sayHi = (person: Human): string => {
   return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!!!`;
 };
 
-console.log(sayHi(person));
+console.log(sayHi(eric));
 
 export {};
